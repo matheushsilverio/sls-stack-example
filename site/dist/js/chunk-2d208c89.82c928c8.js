f428576c354aea878be8aadc2ccd987312c8403e
Matheus <m.silverio@g4educacao.com>
2022-06-09 18:36:53 -0300

    🚀 first commit

---
+++ site/dist/js/chunk-2d208c89.82c928c8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d208c89"],{a5ed:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("p",[n._v("Manutencao")])},u=[],a={},l=a,o=t("2877"),r=Object(o["a"])(l,c,u,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d208c89.82c928c8.js.map
